5fd622907662971e9dc718e7c23996211915603a
Stijn Zanders <zandersstijn@gmail.com>
2020-11-11 23:44:42 +0200

    Add initial up and downstream setting plus unit tests

--- core/models/operator.py
+++ core/models/operator.py
@@ -2,4 +2,30 @@
 
 class Operator:
 
-    pass
+    downstream_tasks = []
+    upstream_tasks = []
+
+
+    def __lshift__(self, task2):
+        """Implements Task << Task"""
+        self.set_upstream(task2)
+
+    def __rshift__(self, task2):
+        """Implements Task >> Task"""
+        self.set_downstream(task2)
+
+    def set_upstream(self, tasks):
+
+        if isinstance(tasks, Operator):
+            tasks = [tasks]
+
+        self.upstream_tasks = [task.task_id for task in tasks]
+
+    def set_downstream(self, tasks):
+
+        if isinstance(tasks, Operator):
+            tasks = [tasks]
+
+        self.downstream_tasks = [task.task_id for task in tasks]
+
+        pass
