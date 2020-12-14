bc2156d21532760e6b4fd82e3479c3a06a61b593
Stijn Zanders <zandersstijn@gmail.com>
2020-12-14 20:28:11 +0000

    Bump version: 0.0.23 → 0.0.24

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open ("README.md", "r") as readme:
 
 setuptools.setup(
     name="limber",
-    version="0.0.23",
+    version="0.0.24",
     entry_points={
             'console_scripts': ['limber=limber.__main__:cli'],
         },
